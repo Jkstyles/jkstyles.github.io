0acad9d4db07724bab0fdc6cc4e9e53ae18a5765
Jake <basstyles@gmail.com>
2018-06-22 13:33:33 -0400

    drop down not up

---
+++ toggle.js
@@ -0,0 +1,10 @@
+function show(id) {
+    var element = document.getElementById(id);
+    element.classList.remove("hidden");
+
+}
+
+function hide() {
+    var element = document.getElementById("first");
+    element.classList.add("one");
+}
