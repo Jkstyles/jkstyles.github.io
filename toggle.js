08929e8acf8b3a7273fbbb294bce527983a8043d
Jake <basstyles@gmail.com>
2018-06-23 18:13:21 -0400

    menu

--- toggle.js
+++ toggle.js
@@ -1,6 +1,24 @@
 function show(id) {
-    var element = document.getElementById(id);
-    element.classList.remove("hidden");
+
+    // var divsToHide = document.getElementsByClassName('bioBox')
+
+    // if(divsToHide.length > 0){
+    //     //divsToHide.classlist.add("hidden")
+
+    //     divsToHide.forEach((div, index) => {
+
+    //         div.classList.add("hidden")
+
+    //     })
+
+    // }
+
+    document.getElementById('first').classList.add('hidden')
+    document.getElementById('second').classList.add('hidden')
+    document.getElementById('third').classList.add('hidden')
+
+    var divToShow = document.getElementById(id);
+    divToShow.classList.remove("hidden");
 
 }
 
