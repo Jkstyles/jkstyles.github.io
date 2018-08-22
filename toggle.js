d5ddb0943e836e558d1474369b6c979ae0e5d015
Jake <basstyles@gmail.com>
2018-08-22 16:17:39 -0400

    slider buttons and dropdown carat

--- toggle.js
+++ toggle.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = 'Greenup Vermont Dashboard \n Written with HTML, CSS and javascript, we were tasked with finding a solution that would allow organizers of Greenup Day easy access to Greenup Vermont\'s collected app data.';
-var speed = 30;
+var speed = 25;
 
 function typeWriter() {
   if (i < txt.length) {
