4ad554acea5affe0df4a33173026d2eea4b300df
Jake <basstyles@gmail.com>
2018-08-22 00:04:16 -0400

    grid fix

---
+++ projectsObject.js
@@ -0,0 +1,39 @@
+class Projects {
+    type: games,
+    name: "Lost In Vermont",
+    description: "A game built at the Burlington Code Academy to show our skills in using map API/'s, memory cache and general design skills"  
+    image:
+    link:
+
+    type:  
+    name:
+    description:
+    image:
+    link:
+
+    type:  
+    name:
+    description:
+    image:
+    link:
+
+    type:  
+    name:
+    description:
+    image:
+    link:
+
+    type:  
+    name:
+    description:
+    image:
+    link:
+
+    type:  
+    name:
+    description:
+    image:
+    link:
+
+
+}
