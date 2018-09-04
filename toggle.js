edcc1b8e53ebdd49eeeea3394f81d92d54a46b92
Jake <basstyles@gmail.com>
2018-09-04 16:07:25 -0400

    margin and font fix

--- toggle.js
+++ toggle.js
@@ -2,6 +2,7 @@ var i = 0;
 var txt = 'Greenup Vermont Dashboard \n Written with HTML, CSS and javascript. We were tasked with finding a solution that would allow organizers of Greenup Day easy access to Greenup Vermont\'s collected app data.';
 var speed = 25;
 
+
 function typeWriter() {
   if (i < txt.length) {
     document.getElementById("typeWriter").innerHTML += txt.charAt(i);
